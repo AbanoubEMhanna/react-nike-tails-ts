b66e175022bd4301f49b44e4b960eaa07b6c4acc
AbanoubEMhanna <62773645+AbanoubEMhanna@users.noreply.github.com>
2024-02-15 02:10:27 +0200

    enhancements

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { FC } from "react";
-import "./App.css";
 
 const App: FC = () => {
   return <div className="text-2xl text-teal-100 text-center rounded-md bg-blue-400 mx-6 mt-10">Hello world</div>;
